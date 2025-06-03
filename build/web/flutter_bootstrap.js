8187fdd784a41c0f2821c80cdb0622fce0725225
Edilfson <mcaisbeda@gmail.com>
2025-06-03 18:48:32 +0300

    Fix shared_preferences for Flutter web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2272981463"
+    serviceWorkerVersion: "2528992695"
   }
 });
