ce24967b4a8298e28aa0d512975132a7ba6f1634
Edilfson <mcaisbeda@gmail.com>
2025-06-03 19:33:51 +0300

    Tüm güncellemeler: merkezi oda/katılımcı yönetimi, shared_preferences web fix, UI ve sunucu güncellemeleri

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2528992695"
+    serviceWorkerVersion: "858128486"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -30,15 +30,15 @@ const RESOURCES = {"assets/AssetManifest.bin": "fbb4cd0ba2fd4dba61867081e8742dd9
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "95bf39d9fa4dc5ca20e3769e475f5082",
+"flutter_bootstrap.js": "dd4c64bf4537223872a358d57617124c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "33e1ce8a981401b0769e914c611d5610",
 "/": "33e1ce8a981401b0769e914c611d5610",
-"main.dart.js": "4de78beb2a3e4a1c22cfb4c8d9c1d819",
-"manifest.json": "5188d0755e138bd47dc32ce99716d603",
+"main.dart.js": "f842001543d459a7c60e677de444a472",
+"manifest.json": "a892c8e206fbdf3615c04ed50d2ac41d",
 "version.json": "949b770f807a55f9a6db4187166cb61b"};
 // The application shell files that are downloaded before a service worker can
 // start.
